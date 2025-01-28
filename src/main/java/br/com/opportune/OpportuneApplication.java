3fddf7685c7fb75c1d88946a11b2b54abeda4405
lucasmanhaesr <ramosmanhaes@outlook.com>
2025-01-28 10:32:31 -0300

    Create repo

---
+++ src/main/java/br/com/opportune/OpportuneApplication.java
@@ -0,0 +1,13 @@
+package br.com.opportune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpportuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/opportune/OpportuneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.opportune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
